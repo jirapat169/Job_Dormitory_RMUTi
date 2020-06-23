836715ab8b8dace3b7d9557adc2ad5171819692f
jirapat169 <james.jirapat@outlook.com>
2020-06-23 20:20:45 +0700

    update student-cost

--- src/app/pages/admin/student-cost/dormitory/dormitory.component.ts
+++ src/app/pages/admin/student-cost/dormitory/dormitory.component.ts
@@ -8,6 +8,7 @@ import { AppService } from 'src/app/services/app.service';
 })
 export class DormitoryComponent implements OnInit {
   public studentSearchData: any = null;
+  public costValue: Array<any> = [];
 
   constructor(public service: AppService) {
     this.service.setHeaderPage('student-cost/dormitory', 'ค่าใช้จ่ายหอพัก');
@@ -25,6 +26,7 @@ export class DormitoryComponent implements OnInit {
       .then((value: any) => {
         if (value.success) {
           if (value.rowCount > 0) {
+            this.getCostValue();
             this.studentSearchData = value.result[0];
             console.log(this.studentSearchData);
           } else {
@@ -32,5 +34,27 @@ export class DormitoryComponent implements OnInit {
           }
         }
       });
+  };
+
+  private getCostValue = () => {
+    this.costValue = [];
+    this.service
+      .httpGet(
+        `/admin/getCostValue?token=${this.service.getUserLogin()['token']}`
+      )
+      .then((value: any) => {
+        if (value) {
+          if (value.rowCount > 0) {
+            this.costValue = value.result;
+          }
+        }
+        console.log(this.costValue);
+      });
+  };
+
+  public searchCost = (term: string, room_type: string) => {
+    return this.costValue.filter((el) => {
+      return el.term == term && el.room_type == room_type;
+    });
   };
 }
