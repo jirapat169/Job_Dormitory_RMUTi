8e4958ab6ff37496b1637b5a254715170e425e38
jirapat169 <james.jirapat@outlook.com>
2020-08-01 03:40:09 +0700

    Update Cost Manage

--- src/app/pages/admin/cost-manage/air/air.component.ts
+++ src/app/pages/admin/cost-manage/air/air.component.ts
@@ -1,15 +1,47 @@
+import { AppService } from './../../../../services/app.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-air',
   templateUrl: './air.component.html',
-  styleUrls: ['./air.component.scss']
+  styleUrls: ['./air.component.scss'],
 })
 export class AirComponent implements OnInit {
+  public formCostTerm1: FormGroup;
+  public formCostTerm2: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, public service: AppService) {}
 
   ngOnInit(): void {
+    this.formCostTerm1 = this.formBuilder.group({
+      dormitory: ['', Validators.required],
+      electric_first: ['', Validators.required],
+      electric_unit: ['', Validators.required],
+      id: ['', Validators.required],
+      insurance: ['', Validators.required],
+      room_type: ['', Validators.required],
+      term: ['', Validators.required],
+      water_first: ['', Validators.required],
+    });
+
+    this.formCostTerm2 = this.formBuilder.group({
+      dormitory: ['', Validators.required],
+      electric_first: ['', Validators.required],
+      electric_unit: ['', Validators.required],
+      id: ['', Validators.required],
+      insurance: ['', Validators.required],
+      room_type: ['', Validators.required],
+      term: ['', Validators.required],
+      water_first: ['', Validators.required],
+    });
   }
 
+  public onSubmitTerm1 = () => {
+    console.log(this.formCostTerm1.value);
+  };
+
+  public onSubmitTerm2 = () => {
+    console.log(this.formCostTerm2.value);
+  };
 }
--- src/app/pages/admin/cost-manage/fan/fan.component.ts
+++ src/app/pages/admin/cost-manage/fan/fan.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AppService } from 'src/app/services/app.service';
 
 @Component({
   selector: 'app-fan',
   templateUrl: './fan.component.html',
-  styleUrls: ['./fan.component.scss']
+  styleUrls: ['./fan.component.scss'],
 })
 export class FanComponent implements OnInit {
+  public formCostTerm1: FormGroup;
+  public formCostTerm2: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, public service: AppService) {}
 
   ngOnInit(): void {
+    this.formCostTerm1 = this.formBuilder.group({
+      dormitory: ['', Validators.required],
+      electric_first: ['', Validators.required],
+      electric_unit: ['', Validators.required],
+      id: ['', Validators.required],
+      insurance: ['', Validators.required],
+      room_type: ['', Validators.required],
+      term: ['', Validators.required],
+      water_first: ['', Validators.required],
+    });
+
+    this.formCostTerm2 = this.formBuilder.group({
+      dormitory: ['', Validators.required],
+      electric_first: ['', Validators.required],
+      electric_unit: ['', Validators.required],
+      id: ['', Validators.required],
+      insurance: ['', Validators.required],
+      room_type: ['', Validators.required],
+      term: ['', Validators.required],
+      water_first: ['', Validators.required],
+    });
   }
 
+  public onSubmitTerm1 = () => {
+    console.log(this.formCostTerm1.value);
+  };
+
+  public onSubmitTerm2 = () => {
+    console.log(this.formCostTerm2.value);
+  };
 }
