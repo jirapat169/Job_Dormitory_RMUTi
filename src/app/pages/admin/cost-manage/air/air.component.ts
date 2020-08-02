0a9a2250ced9054ca61245db71bcc1ef795eb0e3
jirapat169 <james.jirapat@outlook.com>
2020-08-02 19:46:13 +0700

    Update 1-8-20

--- src/app/pages/admin/cost-manage/air/air.component.ts
+++ src/app/pages/admin/cost-manage/air/air.component.ts
@@ -11,7 +11,12 @@ export class AirComponent implements OnInit {
   public formCostTerm1: FormGroup;
   public formCostTerm2: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, public service: AppService) {}
+  constructor(private formBuilder: FormBuilder, public service: AppService) {
+    this.service.setHeaderPage(
+      'cost-manage/air',
+      'จัดการรายการค่าใช้จ่าย (ห้องแอร์)'
+    );
+  }
 
   ngOnInit(): void {
     this.formCostTerm1 = this.formBuilder.group({
@@ -35,13 +40,72 @@ export class AirComponent implements OnInit {
       term: ['', Validators.required],
       water_first: ['', Validators.required],
     });
+
+    this.onLoadData();
   }
 
+  private onLoadData = () => {
+    this.service
+      .httpGet(
+        `admin/getCostValue?token=${this.service.getUserLogin()['token']}`
+      )
+      .then((val: any) => {
+        if (val.rowCount > 0) {
+          val.result
+            .filter((e) => {
+              return e.room_type == '2';
+            })
+            .forEach((element) => {
+              if (element.term == '1')
+                this.formCostTerm1.patchValue({ ...element });
+              else if (element.term == '2')
+                this.formCostTerm2.patchValue({ ...element });
+            });
+        }
+      });
+  };
+
   public onSubmitTerm1 = () => {
-    console.log(this.formCostTerm1.value);
+    this.service
+      .showConfirm(`ยืนยันการบันทึก`, `ภาคเรียนที่ 1`, 'warning')
+      .then((val) => {
+        if (val) {
+          this.service
+            .httpPost(
+              `admin/setCostValue?token=${
+                this.service.getUserLogin()['token']
+              }`,
+              JSON.stringify(this.formCostTerm1.value)
+            )
+            .then((val) => {
+              console.log(val);
+            })
+            .finally(() => {
+              this.onLoadData();
+            });
+        }
+      });
   };
 
   public onSubmitTerm2 = () => {
-    console.log(this.formCostTerm2.value);
+    this.service
+      .showConfirm(`ยืนยันการบันทึก`, `ภาคเรียนที่ 2`, 'warning')
+      .then((val) => {
+        if (val) {
+          this.service
+            .httpPost(
+              `admin/setCostValue?token=${
+                this.service.getUserLogin()['token']
+              }`,
+              JSON.stringify(this.formCostTerm2.value)
+            )
+            .then((val) => {
+              console.log(val);
+            })
+            .finally(() => {
+              this.onLoadData();
+            });
+        }
+      });
   };
 }
--- src/app/pages/admin/cost-manage/fan/fan.component.ts
+++ src/app/pages/admin/cost-manage/fan/fan.component.ts
@@ -11,7 +11,12 @@ export class FanComponent implements OnInit {
   public formCostTerm1: FormGroup;
   public formCostTerm2: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, public service: AppService) {}
+  constructor(private formBuilder: FormBuilder, public service: AppService) {
+    this.service.setHeaderPage(
+      'cost-manage/fan',
+      'จัดการรายการค่าใช้จ่าย (ห้องพัดลม)'
+    );
+  }
 
   ngOnInit(): void {
     this.formCostTerm1 = this.formBuilder.group({
@@ -35,13 +40,73 @@ export class FanComponent implements OnInit {
       term: ['', Validators.required],
       water_first: ['', Validators.required],
     });
+
+    this.onLoadData();
   }
 
+  private onLoadData = () => {
+    this.service
+      .httpGet(
+        `admin/getCostValue?token=${this.service.getUserLogin()['token']}`
+      )
+      .then((val: any) => {
+        console.log(val);
+        if (val.rowCount > 0) {
+          val.result
+            .filter((e) => {
+              return e.room_type == '1';
+            })
+            .forEach((element) => {
+              if (element.term == '1')
+                this.formCostTerm1.patchValue({ ...element });
+              else if (element.term == '2')
+                this.formCostTerm2.patchValue({ ...element });
+            });
+        }
+      });
+  };
+
   public onSubmitTerm1 = () => {
-    console.log(this.formCostTerm1.value);
+    this.service
+      .showConfirm(`ยืนยันการบันทึก`, `ภาคเรียนที่ 1`, 'warning')
+      .then((val) => {
+        if (val) {
+          this.service
+            .httpPost(
+              `admin/setCostValue?token=${
+                this.service.getUserLogin()['token']
+              }`,
+              JSON.stringify(this.formCostTerm1.value)
+            )
+            .then((val) => {
+              console.log(val);
+            })
+            .finally(() => {
+              this.onLoadData();
+            });
+        }
+      });
   };
 
   public onSubmitTerm2 = () => {
-    console.log(this.formCostTerm2.value);
+    this.service
+      .showConfirm(`ยืนยันการบันทึก`, `ภาคเรียนที่ 2`, 'warning')
+      .then((val) => {
+        if (val) {
+          this.service
+            .httpPost(
+              `admin/setCostValue?token=${
+                this.service.getUserLogin()['token']
+              }`,
+              JSON.stringify(this.formCostTerm2.value)
+            )
+            .then((val) => {
+              console.log(val);
+            })
+            .finally(() => {
+              this.onLoadData();
+            });
+        }
+      });
   };
 }
