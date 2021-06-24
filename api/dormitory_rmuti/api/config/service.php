9fbf7f2e3d4b85ba53f3cf31cc3ac9be776efd75
jirapatsk <james.jirapat@gmail.com>
2021-06-24 22:52:28 +0700

    Update API

--- api/dormitory_rmuti/api/config/service.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('DB_SERVERNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'dormitory');
-define('JWT_KEY', 'dormitory_cpe@rmuti1234');
